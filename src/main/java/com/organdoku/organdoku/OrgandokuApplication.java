65a263a3326e52b89afcb05bdc2e9fe516e5f3e6
Melih ŞEN <melihsensakarya@gmail.com>
2021-10-16 16:15:49 +0300

    initial web project

---
+++ src/main/java/com/organdoku/organdoku/OrgandokuApplication.java
@@ -0,0 +1,13 @@
+package com.organdoku.organdoku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgandokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgandokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/organdoku/organdoku/OrgandokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.organdoku.organdoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgandokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
